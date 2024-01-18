fa68209b01bba6c070143cd16f0c09d14aba8534
amadejpapez <amadej.papez@icloud.com>
2024-01-19 00:41:51 +0100

    feat: add all needed input fields for new accident

--- app/src/main/java/com/segmentationfault/saferoute/fragment/NewAccidentFragment.kt
+++ app/src/main/java/com/segmentationfault/saferoute/fragment/NewAccidentFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Base64
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
@@ -75,6 +76,15 @@ class NewAccidentFragment : Fragment(R.layout.fragment_new_accident) {
             binding.captureImage.rotation = 90f
 
             requestDataFromApi(uri)
+        }
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.accident_type_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinner.adapter = adapter
         }
     }
 
